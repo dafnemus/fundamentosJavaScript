ecc5644d12956afe77a617f8a5b1c6204ff92189
dafnemus <dafnemus@gmail.com>
2021-07-12 17:29:57 -0300

    copia de un objeto al que le incremento la edad en 1

---
+++ Objetos/Clase8/clase8.js
@@ -0,0 +1,29 @@
+var dafne = {
+    nombre: 'Dafne',
+    apellido: 'Musante',
+    edad: 23
+}
+
+var dario = {
+    nombre: 'Darío',
+    apellido: 'Lopilato',
+    edad: 35
+}
+/*
+objeto modificado directamente
+function crecer(persona) {
+    perona.edad += 1
+}
+crecer(dafne) => dafne.edad = 24
+*/
+
+// copia de objeto
+function crecer(persona) {
+    return {
+        ...persona,
+        edad: persona.edad += 1
+    }
+}
+
+crecer(dafne)
+crecer(dario)
