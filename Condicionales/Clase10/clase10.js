d4f05a8c2b6f87dcc6991bd0540769e092b20225
dafnemus <dafnemus@gmail.com>
2021-07-12 18:17:39 -0300

    creo la funcion imprimirProfesiones y la funcion esMayorDeEdad

---
+++ Condicionales/Clase10/clase10.js
@@ -0,0 +1,42 @@
+var dafne = {
+    nombre: 'Dafne',
+    apellido: 'Musante',
+    edad: 23,
+    ingeniere: false,
+    cocinere: true,
+    cantante: false,
+    estudiante: true
+}
+
+function imprimirProfesiones(persona) {
+    console.log(`${persona.nombre} es: `)
+
+    if (persona.ingeniere) {
+        console.log('Ingeniere')
+    }
+
+    if (persona.cocinere) {
+        console.log('Cocinere')
+    }
+
+    if (persona.cantante) {
+        console.log('Cantante')
+    }
+
+    if (persona.estudiante) {
+        console.log('Estudiante')
+    }
+}
+
+imprimirProfesiones(dafne)
+
+function EsMayorDeEdad({ nombre, edad }) {
+    if (edad >= 18) {
+        console.log(`${nombre} es mayor de edad.`)
+    } else {
+        console.log(`${nombre} es menor de edad`)
+    }
+    
+}
+
+EsMayorDeEdad(dafne)
