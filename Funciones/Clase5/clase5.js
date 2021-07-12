c8d4fa9993caaf2c93bedfcf6665c6182cc2bd88
dafnemus <dafnemus@gmail.com>
2021-07-12 16:40:09 -0300

    alcance de funciones

---
+++ Funciones/Clase5/clase5.js
@@ -0,0 +1,9 @@
+var nombre1 = 'Dafne'
+
+function imprimirNombreEnMayuscula(nombre) {
+    nombre = nombre.toUpperCase()
+    console.log(nombre)
+}
+
+imprimirNombreEnMayuscula(nombre1)
+console.log(nombre1)
---
+++ Funciones/Clase5/claser.py
@@ -0,0 +1,8 @@
+nombre = 'dafne'
+
+def imprimir_nom_mayus():
+    nombre.title()
+    print(nombre.upper())
+
+imprimir_nom_mayus()
+print(nombre)
