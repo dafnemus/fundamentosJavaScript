9f894c01a5a32025f9e1fd40a5cb6595b31c9f52
dafnemus <dafnemus@gmail.com>
2021-07-12 12:54:45 -0300

    clase 1 variables y console log

---
+++ Variables/clase1.js
@@ -0,0 +1,14 @@
+console.log('Hola Mundo JS')
+
+var nombre = 'Dafne'
+var apellido = 'Musante'
+var edad = 23
+
+// impresión con comas
+console.log('Buen día ', nombre, apellido)
+
+// concatenación
+console.log('Cómo estuvo tu día ' + nombre + ' ' + apellido)
+
+// template
+console.log(`Edad ${edad} años`)
