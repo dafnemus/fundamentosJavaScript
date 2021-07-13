0eb9b9c9e7cda874c572f5a437960b22700d1731
dafnemus <dafnemus@gmail.com>
2021-07-13 17:20:56 -0300

    agrego el metodo calcular altura al prototipo persona

--- POO/Clase21/clase21.js
+++ POO/Clase21/clase21.js
@@ -8,4 +8,12 @@ Persona.prototype.saludar = function () {
   console.log(`Hola! me llamo ${this.nombre} ${this.apellido}`);
 };
 
+const esAlto = (altura) => altura >= 1.8;
+
+Persona.prototype.calcularAltura = function () {
+  esAlto(this.altura)
+    ? console.log(`${this.nombre} es una persona alta.`)
+    : console.log(`${this.nombre} no es una persona alta.`);
+};
+
 var dafne = new Persona("Dafne", "Musante", 1.53);
