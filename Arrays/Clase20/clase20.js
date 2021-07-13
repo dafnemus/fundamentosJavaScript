dd3fa04003cdc68122fa34db809a0538cdcf258f
dafnemus <dafnemus@gmail.com>
2021-07-13 16:29:58 -0300

    aplico destructuring a la func reducer

--- Arrays/Clase20/clase20.js
+++ Arrays/Clase20/clase20.js
@@ -1,59 +1,60 @@
 var sacha = {
-    nombre: 'Sacha',
-    apellido: 'Lifszyc',
-    altura: 1.72,
-    cantidadDeLibros: 111
-}
+  nombre: "Sacha",
+  apellido: "Lifszyc",
+  altura: 1.72,
+  cantidadDeLibros: 111,
+};
 
 var alan = {
-    nombre: 'Alan',
-    apellido: 'Perez',
-    altura: 1.86,
-    cantidadDeLibros: 78
-}
+  nombre: "Alan",
+  apellido: "Perez",
+  altura: 1.86,
+  cantidadDeLibros: 78,
+};
 
 var martin = {
-    nombre: 'Martin',
-    apellido: 'Gomez',
-    altura: 1.85,
-    cantidadDeLibros: 132
-}
+  nombre: "Martin",
+  apellido: "Gomez",
+  altura: 1.85,
+  cantidadDeLibros: 132,
+};
 
 var dario = {
-    nombre: 'Dario',
-    apellido: 'Juarez',
-    altura: 1.71,
-    cantidadDeLibros: 90
-}
+  nombre: "Dario",
+  apellido: "Juarez",
+  altura: 1.71,
+  cantidadDeLibros: 90,
+};
 
 var vicky = {
-    nombre: 'Vicky',
-    apellido: 'Zapata',
-    altura: 1.56,
-    cantidadDeLibros: 91
-}
+  nombre: "Vicky",
+  apellido: "Zapata",
+  altura: 1.56,
+  cantidadDeLibros: 91,
+};
 
 var paula = {
-    nombre: 'Paula',
-    apellido: 'Barros',
-    altura: 1.76,
-    cantidadDeLibros: 182
-}
+  nombre: "Paula",
+  apellido: "Barros",
+  altura: 1.76,
+  cantidadDeLibros: 182,
+};
 
-var personas = [sacha, alan, martin, dario, vicky, paula]
+var personas = [sacha, alan, martin, dario, vicky, paula];
 
 // total libros con for
-var totalLibros = 0
+var totalLibros = 0;
 for (var i = 0; i < personas.length; i++) {
-    totalLibros += personas[i].cantidadDeLibros
+  totalLibros += personas[i].cantidadDeLibros;
 }
 
-console.log(`Las personas juntan un total de ${totalLibros} libros.`)
+console.log(`Las personas juntan un total de ${totalLibros} libros.`);
 
 // con reduce
-var contador = 0
-const reducer = (contador, objeto) => contador += objeto.cantidadDeLibros
+var contador = 0;
+const reducer = (contador, { cantidadDeLibros }) =>
+  (contador += cantidadDeLibros);
 
-var librosTotales = personas.reduce(reducer, 0)
+var librosTotales = personas.reduce(reducer, 0);
 
-console.log(`Las personas juntan un total de ${librosTotales} libros.`)
+console.log(`Las personas juntan un total de ${librosTotales} libros.`);
