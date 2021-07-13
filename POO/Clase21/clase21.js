60d5375f6167c5d1be34309c613cde6f176076d9
dafnemus <dafnemus@gmail.com>
2021-07-13 17:20:16 -0300

    agrego el metodo saludar al prototipo persona

--- POO/Clase21/clase21.js
+++ POO/Clase21/clase21.js
@@ -4,4 +4,8 @@ function Persona(nombre, apellido, altura) {
   this.altura = altura;
 }
 
+Persona.prototype.saludar = function () {
+  console.log(`Hola! me llamo ${this.nombre} ${this.apellido}`);
+};
+
 var dafne = new Persona("Dafne", "Musante", 1.53);
