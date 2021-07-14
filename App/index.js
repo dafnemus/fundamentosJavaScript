bc49460e5e7f3f3fa738916928afd44100ffcb50
dafnemus <dafnemus@gmail.com>
2021-07-14 18:30:27 -0300

    creo la secuancia de colores para el nivel 1

--- App/index.js
+++ App/index.js
@@ -9,6 +9,7 @@ class Juego {
   constructor() {
     this.iniciarJuego();
     this.generarSecuencia();
+    this.pasarDeNivel();
   }
 
   iniciarJuego() {
@@ -26,6 +27,43 @@ class Juego {
     this.secuencia = new Array(10)
       .fill(0)
       .map(() => Math.floor(Math.random() * 4));
+  }
+
+  pasarDeNivel() {
+    this.iluminarSecuencia();
+  }
+
+  convertirNumEnCol(numero) {
+    switch (numero) {
+      case 0:
+        return 'verde';
+      case 1:
+        return 'rojo';
+      case 2:
+        return 'amarillo';
+      case 3:
+        return 'azul';
+    }
+  }
+
+  iluminarSecuencia() {
+    for (let i = 0; i < this.nivel; i++) {
+      let color = this.convertirNumEnCol(this.secuencia[i]);
+      setTimeout(() => {
+        this.iluminarColor(color);
+      }, 1000 * i);
+    }
+  }
+
+  iluminarColor(color) {
+    this.colores[color].classList.add('light');
+    setTimeout(() => {
+      this.apagar(color);
+    }, 350);
+  }
+
+  apagar(color) {
+    this.colores[color].classList.remove('light');
   }
 }
 
