e16f787bb4f24269c61f7601e2bfebcece0c1be8
dafnemus <dafnemus@gmail.com>
2021-07-14 13:17:40 -0300

    agrego mensaje por si se pasa mal el nombre del pokemon

--- Asicronia/Clase28/clase28.js
+++ Asicronia/Clase28/clase28.js
@@ -3,23 +3,27 @@ const POKEMON = ':name';
 
 const opts = { crossDomain: true };
 
+const onPokemonNmeResponse = ({ name }) => console.log(`Hola yo soy ${name}`);
+
+const ErrorNombrePokemon = (nombre) =>
+  console.log(`No se pudo obtener el pokemon ${nombre}`);
+
 function obtenerPokemon(nombre, callback) {
   const url = API_POKEMON + POKEMON.replace(':name', nombre);
 
-  const onPokemonNmeResponse = ({ name }) => {
-    console.log(`Hola yo soy ${name}`);
-    if (callback) {
-      callback();
-    }
-  };
-  $.get(url, opts, onPokemonNmeResponse);
+  $.get(url, opts, callback).fail(() => ErrorNombrePokemon(nombre));
 }
 
 // callbacks hells
-obtenerPokemon('bulbasaur', function () {
-  obtenerPokemon('ivysaur', function () {
-    obtenerPokemon('venusaur', function () {
-      obtenerPokemon('ditto');
+obtenerPokemon('bulbasaur', function (personaje) {
+  onPokemonNmeResponse(personaje);
+  obtenerPokemon('ivysaur', function (personaje) {
+    onPokemonNmeResponse(personaje);
+    obtenerPokemon('venusaur', function (personaje) {
+      onPokemonNmeResponse(personaje);
+      obtenerPokemon('ditt', function (personaje) {
+        onPokemonNmeResponse(personaje);
+      });
     });
   });
 });
