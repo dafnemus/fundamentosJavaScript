7ef670dcc0c1ca5b5021ca4447515398a1832670
dafnemus <dafnemus@gmail.com>
2021-07-14 17:07:50 -0300

    pongo el nivel 1, creo la secuencia, y guardo los colores

--- App/index.js
+++ App/index.js
@@ -1,20 +1,34 @@
-const verde = document.getElementById('verde')
-const rojo = document.getElementById('rojo')
-const amarillo = document.getElementById('amarillo')
-const azul = document.getElementById('azul')
+const verde = document.getElementById('verde');
+const rojo = document.getElementById('rojo');
+const amarillo = document.getElementById('amarillo');
+const azul = document.getElementById('azul');
 
-const btnIniciar = document.getElementById('btnIniciar')
+const btnIniciar = document.getElementById('btnIniciar');
 
 class Juego {
-    constructor() {
-        this.iniciarJuego()
-    }
+  constructor() {
+    this.iniciarJuego();
+    this.generarSecuencia();
+  }
 
-    iniciarJuego() {
-        btnIniciar.classList.add('ocultar')
-    }
+  iniciarJuego() {
+    btnIniciar.classList.add('ocultar');
+    this.nivel = 1;
+    this.colores = {
+      verde,
+      rojo,
+      amarillo,
+      azul,
+    };
+  }
+
+  generarSecuencia() {
+    this.secuencia = new Array(10)
+      .fill(0)
+      .map(() => Math.floor(Math.random() * 4));
+  }
 }
 
 function empezarJuego() {
-    var juego = new Juego()
+  var juego = new Juego();
 }
